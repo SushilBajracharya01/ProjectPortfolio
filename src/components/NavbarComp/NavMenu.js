c8b24dba4597c0fa6e939cca8f84ebb3dff3059f
Sushil <bajracharya.sushil01@gmail.com>
2020-02-07 21:10:03 +0545

    favicon added

--- src/components/NavbarComp/NavMenu.js
+++ src/components/NavbarComp/NavMenu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 
 import NavBtn from './NavBtn';
@@ -7,22 +6,19 @@ import ResumeBtn from './ResumeBtn';
 import { faHome, faUniversity, faUser, faTools, faLaptopCode, faFile, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
-const NavBar = styled.nav`
-    list-style-type: none;
-`;
 
 
 const NavMenu = () => {
     return (
-        <NavBar>
+        <nav className="NavBar">
             <NavBtn href="" navItem="HOME" navIcon={faHome}/>
             <NavBtn href="../About" navItem="ABOUT" navIcon={faUser} />
             <NavBtn href="../Skills" navItem="SKILLS" navIcon={faTools} />
             <NavBtn href="../Education" navItem="EDUCATION" navIcon={faUniversity} />
             <NavBtn href="../Project" navItem="PROJECT" navIcon={faLaptopCode} />
-            <ResumeBtn href="https://drive.google.com/file/d/163-Bl8t_ShhwPUdyr0Jf66OWbn4dbI-q/view?usp=sharing" navItem="RESUME" navIcon={faFile} />
+            <ResumeBtn navIcon={faFile} />
             <NavBtn href="../Contact" navItem="CONTACT" navIcon={faEnvelope} />
-        </NavBar>
+        </nav>
     )
 }
 
--- src/components/NavbarComp/ResumeBtn.js
+++ src/components/NavbarComp/ResumeBtn.js
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const ResumeBtn = (props) => {
-    const { href, navIcon, navItem } = props;
+    const { navIcon } = props;
     return (
         <li>
-            <a href={href} className='NavBtn text ' target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={navIcon} /><span className="d-none d-md-block">{navItem}</span>
+            <a href='https://drive.google.com/file/d/1ELwqCp0VpYWvHmO4XPAS69ATKObbbmL6/view?usp=sharing' className='NavBtn text ' target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={navIcon} /><span className="d-none d-md-block">RESUME</span>
             </a>
         </li>
     )
