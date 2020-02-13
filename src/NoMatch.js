1f77065d7cb601a23a7e3c02933e6ef209a7132e
Sushil <bajracharya.sushil01@gmail.com>
2020-02-13 21:53:20 +0545

    No match page completed

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TitleHead } from './components/macroComponents/TitleHead';
-import Error404 from './assets/error404.png';
+import Error404 from './assets/error404.svg';
 
 
 export const NoMatch = () => (
@@ -9,15 +9,30 @@ export const NoMatch = () => (
 
         <div className="container border rounded">
 
-            <div className="row ProjectContainer errorContainer" style={{
+            {/* <div className="row ProjectContainer errorBox" style={{
                 background: `url(${Error404})`
             }}>
                 <div className="error">
-                <TitleHead>ERROR 404</TitleHead>
+                    <div className="display-4 ">ERROR 404</div>
+                    <p className="lead">Page Not Found</p>
+                </div>
+            </div> */}
 
+            <div className="row ProjectContainer">
+                <div className="m-auto">
+                    <span className="d-flex justify-content-center">
+                        <img src={Error404} alt="Error404" className="error404"/>
+                    </span>
+                    <div className="ErrorTxt text-center">
+                        <div className="display-4">
+                            Error 404
+                        </div> 
+                        <div className="lead">
+                            Page Not Found
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
 )
