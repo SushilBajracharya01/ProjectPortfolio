d899e57b9d8065fddd342664a8bf471354c299e5
Sushil <bajracharya.sushil01@gmail.com>
2020-02-13 20:34:22 +0545

    homepage url updated to sushilbajracharaya01.com.np

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { TitleHead } from './components/macroComponents/TitleHead';
+import Error404 from './assets/error404.png';
+
 
 export const NoMatch = () => (
-    <div>
-        NoMatch
+    <div className="px-3">
+        <TitleHead>ERROR</TitleHead>
+
+        <div className="container border rounded">
+
+            <div className="row ProjectContainer errorContainer" style={{
+                background: `url(${Error404})`
+            }}>
+                <div className="error">
+                <TitleHead>ERROR 404</TitleHead>
+
+                </div>
+            </div>
+
+        </div>
     </div>
 )
